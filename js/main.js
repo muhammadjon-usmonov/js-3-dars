64f896b756c6438e0cd4402d9d29b3bfa7f3572c
muhmmadjon-usmonov <muhammadjonusmonov001@gmail.com>
2022-01-11 16:39:28 +0500

    changed

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var elSurveyInput=elSurveyForm.querySelector('.survey__input');
 var elSurveySelect=elSurveyForm.querySelector('.survey__select');
 var elSurveyResult=document.querySelector('.survey__result');
 
-var valuta_usd=1080.00;
+var valuta_usd=10840.00;
 var valuta_euro=12278.49;
 var valuta_rubl=144.65;
 
